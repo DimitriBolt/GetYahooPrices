64924244bac98752b5a36adec073331fccc52eee
Dimitri Bolt <Dmitry@Bolt.Investments>
2022-05-14 14:41:16 -0600

    add choice of iFile and DBTable.

---
+++ src/main/java/investments/Bolt/IsDBTableExists.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package investments.Bolt;
+
+class IsDBTableExists {
+	// static Class variables
+
+	// private Instance variable
+	// Initializer block
+	String exstingDBTableName;
+
+	// Constructors
+	IsDBTableExists(String dbTbleName) {
+		exstingDBTableName = dbTbleName;
+	}
+
+	// Methods
+	// Mutator (= setter) methods
+	// Accessor (= getter) methods
+	String getInstanceVariable() {
+		return "this.InstanceVariable";
+	}
+
+}
