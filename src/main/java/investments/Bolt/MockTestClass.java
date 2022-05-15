a3a352fe14bc06ba26afb141a06fe61d001892a2
Dimitri Bolt <Dmitry@Bolt.Investments>
2022-05-14 22:07:27 -0600

    change Java version in pom.xml .

---
+++ src/main/java/investments/Bolt/MockTestClass.java
@@ -0,0 +1,21 @@
+package investments.Bolt;
+
+public class MockTestClass {
+	// static Class variables
+	// private Instance variable
+	// Initializer block
+	// Constructors
+	MockTestClass(String iFile) {
+	}
+	// Methods
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+	// Mutator (= setter) methods
+	// Accessor (= getter) methods
+	String getInstanceVariable() {
+		return "this.InstanceVariable";
+	}
+
+}
