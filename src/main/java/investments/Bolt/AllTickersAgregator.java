9afb86e29b16d1f1e82bbcc72fc70b8d1f899418
Dimitri Bolt <Dmitry@Bolt.Investments>
2022-05-18 12:24:29 -0600

    catch future.get TimeoutException

--- src/main/java/investments/Bolt/AllTickersAgregator.java
+++ src/main/java/investments/Bolt/AllTickersAgregator.java
@@ -31,31 +31,34 @@ public class AllTickersAgregator {
 			// Запускаем 3000 потоков
 			OneTickerRunnable oneTickerRunnable = new OneTickerRunnable(ticker, iTickers++, this.allTickersMap); // Runnable
 			Future<?> future = executorService.submit(oneTickerRunnable);
-			
+
 			futureSet.add(future);
 		}
 		for (Future<?> future : futureSet) {
 			try {
-				future.get(2,TimeUnit.MINUTES);
+				future.get(2, TimeUnit.MINUTES);
 			} catch (InterruptedException | ExecutionException | TimeoutException e) {
+				// Слава богу здесь срабатывает java.util.concurrent.TimeoutException
+				// TODO но нужно понять, почему он срабатывает и как с этим бороться. 
 				e.printStackTrace();
+				System.out.printf("Class = %s \t| Row=41 | ?? %s  | strJsonUrl = %s\n", this.getClass().getSimpleName(), future.getClass().getSimpleName());
 			}
-		}
+		} // Тут все объединилось и, видимо, пока все не выполнился, дальше не идет. 
 		executorService.shutdown();
 		try {
 			boolean done = executorService.awaitTermination(3, TimeUnit.MINUTES);
+			System.out.printf("Class = %s | row = 49 | executorService.awaitTermination = %s\n\n", this.getClass().getSimpleName(), done);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		// https://habr.com/ru/post/260953/
-		// все потоки должны завершиться. ! Только здесь мы собрали все потоки в кучу!
 		System.out.printf("Class = %s | row = 35 | tickerThreadS.size() = %s | Все потоки запущены, с-join-нены и работают ...\n\n", this.getClass().getSimpleName(), futureSet.size());
 	}// End of constructors
-	// Methods
-	// Accessor (= getter) methods
+		// Methods
+		// Accessor (= getter) methods
 
 	SortedMap<String, OneTickerParser> getAllPrices() {
-		System.out.printf("Class = %s | row = 43 | this.allTickersMap.size() = %s | Все цены получены, все потоки завершились.\n\n", this.getClass().getSimpleName(), this.allTickersMap.size());
+		System.out.printf("Class = %s | row = 61 | this.allTickersMap.size() = %s | Все цены получены, все потоки завершились.\n\n", this.getClass().getSimpleName(), this.allTickersMap.size());
 		return this.allTickersMap;
 	}
 }
